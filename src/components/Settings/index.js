c367c9ad84f9c07edc4663c0b35d3d2bfe332738
Rob Taussig <robert.taussig@gmail.com>
2019-06-19 21:05:49 -0400

    Fix import path

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import useApp from '../hooks/useApp';
+import useApp from '../../hooks/useApp';
 import EditPlan from '../EditPlan';
 
 const styles = theme => ({
