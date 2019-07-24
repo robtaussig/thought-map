74bd03ccecbb6e2020a4d6fcf4158eec74ad2bfe
Rob Taussig <robert.taussig@gmail.com>
2019-07-23 21:52:19 -0400

    Implement auto-suggest

---
+++ src/hooks/useAutoSuggest.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export const useAutoSuggest = (inputValue, historicalEntries) => {
+  const [wordMap, setWordMap] = useState([]);
+  const lastEntries = useRef(null);
+  const markovChain = useRef(null);
+
+  const getAutoSuggest = () => {
+    if (markovChain.current === null) {
+      markovChain.current = new AutoSuggest();
+    }
+    return markovChain.current;
+  };
+
+  useEffect(() => {
+    let nextWordMap;
+
+    if (lastEntries.current === historicalEntries) {
+      nextWordMap = getAutoSuggest().generateSuggestions(inputValue);
+    } else {
+      nextWordMap = getAutoSuggest()
+                      .process(historicalEntries)
+                      .generateSuggestions(inputValue);
+
+      lastEntries.current = historicalEntries;
+    }
+
+    setWordMap(nextWordMap);
+  }, [inputValue, historicalEntries]);
+
+  return wordMap;
+};
+
+class AutoSuggest {
+  constructor() {
+    this.visited = {};
+    this.trie = new Trie();
+    this.markovChain = new MarkovChain();
+  }
+
+  process(historicalEntries) {
+    historicalEntries.forEach(entry => {
+      if (this.visited[entry] !== true) {
+        const splitEntryWithPunctuationRemoved =
+          entry.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").split(' ');
+
+        this.trie.add(splitEntryWithPunctuationRemoved);
+        this.markovChain.record(splitEntryWithPunctuationRemoved);
+        this.visited[entry] = true;
+      }
+    });
+
+    return this
+  }
+
+  generateSuggestions(inputValue) {
+    const results = this.markovChain.suggest(inputValue)
+                             .concat(this.trie.suggest(inputValue));
+    return [...new Set(results)];
+  }
+}
+
+const COMPLETE_WORD = '*  *';
+
+class Trie {
+  constructor() {
+    this.rootNode = {};
+  }
+
+  add(values) {
+    for (let i = 0; i < values.length; i++) {
+      let currentNode = this.rootNode;
+      const word = values[i];
+      for (let j = 0; j < word.length; j++) {
+        const char = word[j];
+        currentNode[char] = currentNode[char] || {};
+        currentNode = currentNode[char];
+      }
+      currentNode[COMPLETE_WORD] = true;
+    }
+  }
+
+  findCompleteWordsAtDepth(wordPrefix, node, depth) {
+    if (depth === 0) {
+      if (node[COMPLETE_WORD]) {
+        return [wordPrefix];
+      };
+      return [];
+    }
+    const suggestions = [];
+    if (node[COMPLETE_WORD]) {
+      suggestions.push(wordPrefix);
+    }
+    for (let char in node) {
+      
+      if (typeof node[char] === 'object') suggestions.push(...this.findCompleteWordsAtDepth(wordPrefix + char, node[char], depth - 1));
+    }
+    return suggestions;
+  }
+
+  suggest(value) {
+    let currentNode = this.rootNode;
+    const suggestions = [];
+    const splitWords = value.split(' ');
+    const lastWord = splitWords[splitWords.length - 1];
+    for (let i = 0; i < lastWord.length; i++) {
+      if (currentNode[lastWord[i]] === undefined) return [];
+      currentNode = currentNode[lastWord[i]];
+    }
+    if (currentNode[COMPLETE_WORD]) {
+      suggestions.push(lastWord);
+    };
+  
+    suggestions.push(...this.findCompleteWordsAtDepth(lastWord, currentNode, 4));
+
+    return suggestions;
+  }
+}
+
+class MarkovChain {
+  constructor() {
+    this.chain = {};
+  }
+
+  record(values) {
+    let currentWord = values[0];
+    for (let i = 1; i < values.length; i++) {
+      const nextWord = values[i];
+      this.chain[currentWord] = this.chain[currentWord] || {};
+      this.chain[currentWord][nextWord] = this.chain[currentWord][nextWord] || 0;
+      this.chain[currentWord][nextWord]++;
+      currentWord = nextWord;
+    }
+  }
+
+  suggest(value) {
+    if (this.chain[value]) {
+      return Object.entries(this.chain[value])
+        .sort(([aKey, aValue], [bKey, bValue]) => aValue > bValue ? -1 : 1)
+        .map(([key, value]) => ` ${key}`);
+    }
+    return [];
+  }
+}
