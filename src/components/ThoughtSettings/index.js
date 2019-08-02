d701f29ca9c2271e8d34ba2b6066d942521f3f45
Rob Taussig <robert.taussig@gmail.com>
2019-08-02 18:56:27 -0400

    Remove draggable window test

--- src/components/ThoughtSettings/index.js
+++ src/components/ThoughtSettings/index.js
@@ -5,15 +5,12 @@ import Delete from '@material-ui/icons/Delete';
 import useModal from '../../hooks/useModal';
 import Template from './components/template';
 import { styles } from './styles';
-import useDraggableWindow from 'react-use-draggable-window';
 
 export const ThoughtSettings = ({ classes, display, thought, tags, notes, onDelete }) => {
   const [openModal, closeModal] = useModal();
-  const openWindow = useDraggableWindow();
 
   const handleClickUseAsTemplate = () => {
-    // openModal(<Template classes={classes} onClose={closeModal} thought={thought} tags={tags} notes={notes}/>, 'Template');
-    openWindow(<Template classes={classes} onClose={closeModal} thought={thought} tags={tags} notes={notes}/>);
+    openModal(<Template classes={classes} onClose={closeModal} thought={thought} tags={tags} notes={notes}/>, 'Template');
   };
 
   const handleClickCustomColor = () => {
