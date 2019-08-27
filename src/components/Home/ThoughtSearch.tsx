1543d5576359e3f343b605dc6fc72067d63f2d65
Rob Taussig <robert.taussig@gmail.com>
2019-08-26 21:52:34 -0400

    Fix scroll

--- src/components/Home/ThoughtSearch.tsx
+++ src/components/Home/ThoughtSearch.tsx
@@ -159,6 +159,7 @@ const styles = (theme: any): StyleRules => ({
     display: 'flex',
     width: '100%',
     backgroundColor: 'white',
+    flex: '0 0 64px',
     overflow: 'hidden',
     borderRadius: 12,
     marginBottom: 30,
