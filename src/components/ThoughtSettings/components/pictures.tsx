2d6d6b8a6ff1dc8fb6b32a21426729631cc09077
Rob Taussig <robert.taussig@gmail.com>
2019-08-27 23:03:51 -0400

    Add imgur support

--- src/components/ThoughtSettings/components/pictures.tsx
+++ src/components/ThoughtSettings/components/pictures.tsx
@@ -16,6 +16,7 @@ interface PictureProps {
 }
 
 const UPLOAD_OPTIONS_WIDTH = 50;
+const IMGUR_CLIENT_ID = 'f1b9f4565330211';
 
 const styles = (theme: any): StyleRules => ({
   root: {
@@ -27,18 +28,11 @@ const styles = (theme: any): StyleRules => ({
   uploadInput: {
     flex: '0 0 40px',
   },
-  header: {
-    position: 'relative',
-  },
   thoughtsContainer: {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
     overflow: 'auto',
-  },
-  uploadHeader: {
-    position: 'absolute',
-    right: 0,
   },
   imageList: {
     display: 'flex',
@@ -83,7 +77,11 @@ export const Pictures: FC<PictureProps> = ({ classes, onClose, thought }) => {
   const { dispatch } = useApp();
   const [pictures, setPictures] = useNestedXReducer('pictures', state, dispatch);
   const [tempImages, setTempImages] = useState<any[]>([]);
-  const relatedPictures = useMemo(() => Object.values(pictures).filter(picture => picture.thoughtId === thought.id),[pictures, thought]);
+  const relatedPictures = useMemo(() =>
+    Object.values(pictures)
+      .filter(picture => picture.thoughtId === thought.id)
+      .sort((a, b) => a.created - b.created)
+  ,[pictures, thought]);
 
   useEffect(() => {
     const handleChange: EventListener = e => {
@@ -105,6 +103,31 @@ export const Pictures: FC<PictureProps> = ({ classes, onClose, thought }) => {
     setTempImages(prev => prev.filter(prevImage => prevImage !== tempImages[idx]));
   };
 
+  const uploadImageToImgur = (idx: number) => async () => {
+    try {
+      const base64: any = await getBase64ImageFromUrl(tempImages[idx]);
+      const res = await fetch('https://api.imgur.com/3/image', {
+        headers: {
+            'Authorization': `Client-ID ${IMGUR_CLIENT_ID}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify({
+              image: base64.replace(/^data.*base64/,''),
+          }),
+      })
+      const rjson = await res.json();
+      await pictureActions.createPicture(db, {
+        imgurUrl: rjson.data.link,
+        thoughtId: thought.id,
+      });
+      setTempImages(prev => prev.filter(prevImage => prevImage !== tempImages[idx]));
+    } catch(e) {
+      alert(e && e.message ? e.message : e);
+    }
+  };
+
   return (
     <div className={classes.root}>
       <label className={classes.uploadInput}>
@@ -112,34 +135,29 @@ export const Pictures: FC<PictureProps> = ({ classes, onClose, thought }) => {
         <input ref={uploadPictureRef} type="file" accept="image/*" id="file-input"/>
       </label>
       <div className={classes.thoughtsContainer}>
-        {tempImages.length > 0 && (<Fragment>
-          <h2 className={classes.header}>Pictures<span className={classes.uploadHeader}>Upload</span></h2>
-          <div className={classes.imageList}>
-            {tempImages.map((objectUrl, idx) => {
-              return (
-                <div key={`${idx}-temp-image`} className={classes.imageItem}>
-                  <img src={objectUrl} className={classes.image}/>
-                  <div className={classes.uploadOptions}>
-                    <button onClick={uploadImageLocally(idx)}>Local</button>
-                    <button>Imgur</button>
-                  </div>
+        {tempImages.length > 0 && (<div className={classes.imageList}>
+          {tempImages.map((objectUrl, idx) => {
+            return (
+              <div key={`${idx}-temp-image`} className={classes.imageItem}>
+                <img src={objectUrl} className={classes.image}/>
+                <div className={classes.uploadOptions}>
+                  <button onClick={uploadImageLocally(idx)}>Local</button>
+                  <button onClick={uploadImageToImgur(idx)}>Imgur</button>
                 </div>
-              );
-            })}
-          </div>
-        </Fragment>)}
-        {relatedPictures.length > 0 && (<Fragment>
-          <div className={classes.imageList}>
-            {relatedPictures.map((picture, idx) => {
-              return (
-                <div key={`${idx}-thought-picture`} className={classes.pictureItem}>
-                  <img src={picture.localUrl || picture.imgurUrl} className={classes.image}/>
-                  <span className={classes.pictureDescription}>{picture.description}</span>
-                </div>
-              );
-            })}
-          </div>
-        </Fragment>)}
+              </div>
+            );
+          })}
+        </div>)}
+        {relatedPictures.length > 0 && (<div className={classes.imageList}>
+          {relatedPictures.map((picture, idx) => {
+            return (
+              <div key={`${idx}-thought-picture`} className={classes.pictureItem}>
+                <img src={picture.localUrl || picture.imgurUrl} className={classes.image}/>
+                <span className={classes.pictureDescription}>{picture.description}</span>
+              </div>
+            );
+          })}
+        </div>)}
       </div>
     </div>
   );
