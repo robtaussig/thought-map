08d3eb7521e16493945f33b477ce902e43599993
Rob Taussig <robert.taussig@gmail.com>
2019-11-21 22:08:15 -0500

    Speed animation

--- src/components/General/CircleButton.tsx
+++ src/components/General/CircleButton.tsx
@@ -48,11 +48,11 @@ export const CircleButton: FC<CircleButtonProps> = ({
     <motion.button
       whileHover={{
         scale: 1.1,
-        transition: { duration: 0.2 },
+        transition: { duration: 0.1 },
       }}
       whileTap={{
         scale: 0.9,
-        transition: { duration: 0.2 },
+        transition: { duration: 0.1 },
       }}
       onTap={handleTap}
 
