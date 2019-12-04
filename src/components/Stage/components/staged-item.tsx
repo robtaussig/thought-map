dc7e18c1c1c68ed5ad0b6191ccaa38e1da38b563
Rob Taussig <robert.taussig@gmail.com>
2019-12-03 21:52:09 -0500

    Make thought title clickable in stage

--- src/components/Stage/components/staged-item.tsx
+++ src/components/Stage/components/staged-item.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Thought } from '../../../store/rxdb/schemas/thought';
 import { thoughts as thoughtActions } from '../../../actions';
 import { useLoadedDB } from '../../../hooks/useDB';
+import useApp from '../../../hooks/useApp';
 import Undo from '@material-ui/icons/Undo';
 import Autorenew from '@material-ui/icons/Autorenew';
 import { format} from 'date-fns';
@@ -14,6 +15,11 @@ interface StagedItemProps {
 
 export const StagedItem: FC<StagedItemProps> = ({ classes, isStaging, item }) => {
   const db = useLoadedDB();
+  const { history } = useApp();
+  const handleClickItem = () => {
+    history.push(`thought/${item.id}`);
+  };
+
   const handleClickUnstage = () => {
     thoughtActions.editThought(db, {
       ...item,
@@ -30,7 +36,7 @@ export const StagedItem: FC<StagedItemProps> = ({ classes, isStaging, item }) =>
 
   return (
     <div className={classes.stagedItem}>
-      <span className={classes.stagedItemTitle}>{item.title}</span>
+      <button className={classes.stagedItemTitle} onClick={handleClickItem}>{item.title}</button>
       <button className={classes.unstageButton} onClick={handleClickUnstage}><Undo/></button>
       <button className={classes.promoteButton} onClick={handleClickPromote} disabled={isStaging}><Autorenew/></button>
     </div>
