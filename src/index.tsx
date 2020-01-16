65252b30f64787de43100b2ab7fbb607b5b158ea
Rob Taussig <robert.taussig@gmail.com>
2020-01-15 23:10:52 -0500

    Add pushRegistry

--- src/index.tsx
+++ src/index.tsx
@@ -38,6 +38,23 @@ if ("serviceWorker" in navigator) {
       })
       .then(function (reg) {
         console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+        reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: 'BIwXEvPsInlDsg5cwhrCmrSc_SMl-VWizfB-BL2h8vuMKWkvYCH6_J29Z-SUcJ3Dx2hnOz9LImtKIyYklIjN5VE'
+        }).then(
+          function (pushSubscription) {
+            console.log(pushSubscription.endpoint);
+            // The push subscription details needed by the application
+            // server are now available, and can be sent to it using,
+            // for example, an XMLHttpRequest.
+          }, function (error) {
+            // During development it often helps to log errors to the
+            // console. In a production environment it might make sense to
+            // also report information about errors back to the
+            // application server.
+            console.log(error);
+          }
+        );
       });
   }
 }
--- src/public/sw.js
+++ src/public/sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "pwabuilder-precache";
+const CACHE = "pwabuilder-precache-with-push-v2";
 const precacheFiles = [
   /* Add an array of files to precache for your app */
 ];
