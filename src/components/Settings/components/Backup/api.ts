29f139955ff54ad1f85e877cbfb77ac7fe2ebaeb
Rob Taussig <robert.taussig@gmail.com>
2020-01-27 20:05:06 -0500

    Add Update method

--- src/components/Settings/components/Backup/api.ts
+++ src/components/Settings/components/Backup/api.ts
@@ -21,6 +21,25 @@ export const uploadChunk = async (chunk: ArrayBuffer, part: number, uuid: string
   return true;
 };
 
+export const updateChunk = async (chunk: ArrayBuffer, part: number, uuid: string, password: string = ''): Promise<any | Error> => {
+  const res = await fetch(`${API}/thought-map/api/backup`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      uuid,
+      part,
+      chunk: ab2str(chunk),
+      password,
+    }),
+  });
+  if (!res.ok) return new Error(res.statusText);
+
+  return true;
+};
+
 export const fetchBackup = async (uuid: string, password: string = ''): Promise<BackupResponse | Error> => {
   const res = await fetch(`${API}/thought-map/api/retrieve-backup/${uuid}?password=${password}`, {
     headers: {
