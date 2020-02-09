da8bb296f4bed2f8fe414d0714c21c0e9fba6e39
Rob Taussig <robert.taussig@gmail.com>
2020-02-08 21:44:08 -0500

    Check for null in getWebSocket

--- src/App.tsx
+++ src/App.tsx
@@ -72,8 +72,8 @@ const App: FC<AppProps> = ({ classes, history }) => {
   useEffect(() => {
     const handleCheckWebSocket = () => {
       if (document.visibilityState === 'visible') {
-        if (getWebSocket().readyState !== ReadyState.OPEN) {
-          (getWebSocket() as any).reconnect?.current?.() ?? alert('This did not work');
+        if (getWebSocket()?.readyState !== ReadyState.OPEN) {
+          (getWebSocket() as any)?.reconnect?.current?.() ?? alert('This did not work');
         }
       }
     }
